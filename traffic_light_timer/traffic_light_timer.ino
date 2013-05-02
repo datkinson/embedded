560b3f1ff042606d774e07e631d729796f307263
Daniel Atkinson <daa9@aber.ac.uk>
2013-05-02 14:32:49 +0100

    Added traffic light timer project

---
+++ traffic_light_timer/traffic_light_timer.ino
@@ -0,0 +1,15 @@
+//define button input pins
+const int demoButton = 4;
+const int resetButton = 3;
+const int startButton = 2;
+
+void setup()
+{
+  //initialise Serial connection
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+}
